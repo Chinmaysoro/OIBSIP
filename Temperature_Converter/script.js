393a71bc00c220cf2fad485b58a0fb06b3342895
Chinmay <Chinmay.ons@gmail.com>
2023-12-28 12:31:36 +0530

    Project finalised...

--- Temperature_Converter/script.js
+++ Temperature_Converter/script.js
@@ -1,25 +1,32 @@
 function convert() {
-  var receivedVal = document.getElementById("received-val").value;
+  var receivedInputVal = document.getElementById("received-val");
   var firstUnitVal = document.getElementById("first-unit").value;
   var secondUnitVal = document.getElementById("second-unit").value;
   var convertedValue = 0;
 
+  var receivedVal = Number(receivedInputVal.value);
+  if (isNaN(receivedVal)){
+    document.getElementById("result").innerHTML = "Invalid input";
+    return;
+  }
+  //Above function rectifies inputs from unrelevent values...
+
   if(firstUnitVal == secondUnitVal){
     convertedValue = receivedVal *1;
   }else if(firstUnitVal == "celsius" && secondUnitVal == "fahrenheit"){
-    convertedValue = (eval(receivedVal)*1.8)+32;
+    convertedValue = (receivedVal*1.8)+32;
   }else if(firstUnitVal == "celsius" && secondUnitVal == "kelvin"){
-    convertedValue = eval(receivedVal)+273.15;
+    convertedValue = receivedVal+273.15;
   }else if(firstUnitVal == "fahrenheit" && secondUnitVal == "celsius"){
-    convertedValue = (eval(receivedVal)-32)*0.5556;
+    convertedValue = (receivedVal-32)*0.5556;
   }else if(firstUnitVal == "fahrenheit" && secondUnitVal == "kelvin"){
-    convertedValue = ((eval(receivedVal)-32)*0.5556)+273.15;
+    convertedValue = ((receivedVal-32)*0.5556)+273.15;
   }else if(firstUnitVal == "kelvin" && secondUnitVal == "celsius"){
-    convertedValue = eval(receivedVal)-273.15;
+    convertedValue = receivedVal-273.15;
   }else if(firstUnitVal == "kelvin" && secondUnitVal == "fahrenheit"){
-    convertedValue = ((eval(receivedVal)-273.15)*1.8)+32;
+    convertedValue = ((receivedVal-273.15)*1.8)+32;
   }else{
     convertedValue = 0;
   }
-  document.getElementById("result").innerHTML = `${convertedValue || 0} ${secondUnitVal || ""}`;
+  document.getElementById("result").innerHTML = `${convertedValue.toFixed(4)} ${secondUnitVal}`;
 }
