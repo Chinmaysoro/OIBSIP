c6b6ed4d419331c68b7d21abb2ffcc334a26585f
Chinmay <Chinmay.ons@gmail.com>
2024-01-06 12:11:27 +0530

    Under development and finalization pending

---
+++ Landing_page/script.js
@@ -0,0 +1,9 @@
+function logclick(){
+  alert("Login page will be available soon...")
+}
+function regclick(){
+  alert("Registration will be available soon...")
+}
+function booknow(){
+  alert("Online Booking disabled. Please call to book your appointment.")
+}
